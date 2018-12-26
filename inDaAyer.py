03ae1625b1fdc987d098eb7e90db55c57eaa1f4b
Jacob Nehama <jacobnehama@Jacobs-MacBook-Pro.local>
2018-12-25 23:47:41 -0600

    successfully add a passenger and their choice of airlines program

--- inDaAyer.py
+++ inDaAyer.py
@@ -4,27 +4,40 @@ import sys
 
 db = SqliteDatabase('Passenger_2ndtry.db')
 
-class Passenger(Model):
+class Passenger1(Model):
 
     passenger = CharField(max_length=255, unique=True)
+    program = CharField(max_length=255)
 
     class Meta:
         database = db
 def initialize():
     db.connect()
-    db.create_tables([Passenger])
-# def join_program():
+    db.create_tables([Passenger1])
+def join_program(passeng) :
+    # program_choice = input("Select a frequent flyer travel program to join: \n1.Delta\n2.American")
+    """join a program"""
+    print("Select a frequent flyer travel program to join: \n1.Delta\n2.American")
+    data = sys.stdin.read().strip()  # read all of the data that comes in
+    # data = input("Enter traveler name: ")
+    if data:
+        inp = input("save entry y or n ").lower()
+        if inp != "n":
+            Passenger1.create(passenger=passeng,program=data)
+            print("prgram saved succesfully")
+
 
 def create_passenger():
     """add a passenger"""
     print("Enter traveler name: ")
     data = sys.stdin.read().strip()  # read all of the data that comes in
     # data = input("Enter traveler name: ")
-    if data:
-        inp = input("save entry y or n ").lower()
-        if inp != "n":
-            Passenger.create(passenger=data)
-            print("saved succesfully")
+    # if data:
+    #     inp = input("save entry y or n ").lower()
+    #     if inp != "n":
+    #         Passenger1.create(passenger=data)
+    #         print("passenger saved succesfully")
+    join_program(data)
 
 def menu_loop():
         menuchoice = menuChoice = input("    1.Create Traveler\n    2.Purchase Travel\n    3.Quit\n  >")
