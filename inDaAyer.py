367fdd650e7dcbbf8130306a9ce3de3e718d2331
Jacob Nehama <jacobnehama@Jacobs-MacBook-Pro.local>
2018-12-27 13:52:48 -0600

    # add looping abilities to the adding of a program so they can have 2 programs# #add a program, ask if theyre done adding a program# #if done, create the entry with name,program1 info, program2 info = to nothing# #if not done, ask for second program choice# # are you finished selecting a program loop# # print membership details with 1.name 2. programs 3. hotel memb numbers#

--- inDaAyer.py
+++ inDaAyer.py
@@ -2,34 +2,111 @@ from peewee import *
 from collections import OrderedDict
 import sys
 
-db = SqliteDatabase('Passenger_2ndtry.db')
+db = SqliteDatabase('Passenger_3rdtry.db')
 
 class Passenger1(Model):
 
     passenger = CharField(max_length=255, unique=True)
     program = CharField(max_length=255)
+    program2 = CharField(max_length=255)
 
     class Meta:
         database = db
 def initialize():
     db.connect()
     db.create_tables([Passenger1])
+
+
 def join_program(passeng) :
     # program_choice = input("Select a frequent flyer travel program to join: \n1.Delta\n2.American")
-    """join a program"""
-    print("Select a frequent flyer travel program to join: \n1.Delta\n2.American")
+    """join program(s)"""
+
+    print("Select a frequent flyer travel program to join: \n1.Delta\n2.American"
+          "\nPress control-D when finished")
     data = sys.stdin.read().strip()  # read all of the data that comes in
     # data = input("Enter traveler name: ")
+    null = True
     if data:
-        inp = input("save entry y or n ").lower()
-        if inp != "n":
-            Passenger1.create(passenger=passeng,program=data)
-            print("prgram saved succesfully")
+        add_another_program = input("do you want to add another program? y/n")
+        if add_another_program == "n":
+            try:
+                inp = input("save entry y or n ").lower()
+                if inp != "n":
+                    Passenger1.create(passenger=passeng,program=data,program2=null)
+                    print("prgram saved succesfully")
+            except IntegrityError:
+                print("hit integ error")
+                pass
+        added_new_passenger(passeng,data,None)
+        if add_another_program == "y":
+            print("Select a frequent flyer travel program to join: \n1.Delta\n2.American"
+                  "\nPress control-D when finished")
+            data2 = sys.stdin.read().strip()  # read all of the data that comes in
+            if data2:
+                try:
+                    inp = input("save entry y or n ").lower()
+                    if inp != "n":
+                        Passenger1.create(passenger=passeng, program=data, program2=data2)
+                        print("Program 2 saved succesfully")
+                except IntegrityError:
+                    print("hit integ error")
+                    pass
+            added_new_passenger(passeng,data,data2)
+def added_new_passenger(passeng,data,data2):
+    if data == "1":
+        data = "Delta Airlines Frequent Flier Program"
+    if data == "2":
+        data = "American Airlines Frequent Flier Program"
+    if data2 == "1":
+        data2 = "Delta Airlines Frequent Flier Program"
+    if data2 == "2":
+        data2 = "American Airlines Frequent Flier Program"
+    print("Added new traveler")
+    print("Name: {}".format(passeng))
+    student = Passenger1.select().order_by(Passenger1.id.desc()).get() #sort Passenger1.id's in desc order, get the first one
+    print("Memberships: \n{}\nMembership Number = {}\n{}\nMembership Number = {}".format(data,student,data2,student))
+    # id_num = Passenger1.select()
+    # for id_nums in id_num:
+    #     print("ID NUMBER")
+    #     print(id_nums.id)
+    # student2 = Passenger1.select().order_by(Passenger1.passenger.get()) #sort Passenger1.id's in desc order, get the first one
+    # print(student2)
+    list_of_passengers = Passenger1.select(Passenger1)
+    for passengers in list_of_passengers:
+        print(passengers.passenger)
+
+
+
+
+def view_entries(search_query = None):
+    """View prev entries"""
+    entries = Passenger1.select().order_by(Passenger1.timestamp.desc())
+    # view entries in order of descending timestamp
+    if search_query:
+        entries = entries.where(Passenger1.content.contain(search_query))
+        # filters so all the entries we select have the content search in their attirbute
+    for entry in entries:
+        timestamp = entry.timestamp.strftime("%A,%B,%d,%Y,%I,%M,%p")
+        #weekday name, month name, number,year,hour,minute,am or pm
+        print(timestamp)
+        print(entry.content)
+        print("n)next entry")
+        print("d)del entry")
+
+        print("q)return to menu")
+
+
+
+def search_entries():
+    """Search entries for a string"""
+    view_entries(input("search query"))
+
 
 
 def create_passenger():
     """add a passenger"""
-    print("Enter traveler name: ")
+    print("Enter traveler name: "
+          "\nPress control-D when finished")
     data = sys.stdin.read().strip()  # read all of the data that comes in
     # data = input("Enter traveler name: ")
     # if data:
@@ -39,7 +116,10 @@ def create_passenger():
     #         print("passenger saved succesfully")
     join_program(data)
 
+
 def menu_loop():
+    while True:
+
         menuchoice = menuChoice = input("    1.Create Traveler\n    2.Purchase Travel\n    3.Quit\n  >")
         if menuChoice == "1":
            create_passenger()
@@ -48,8 +128,8 @@ def menu_loop():
         if menuChoice == 3:
             print("cya")
             exit()
-        else:
-            print("value is out of range")
+        # else:
+        #     print("value is out of range")
 
 if __name__ == '__main__':
     initialize()
@@ -57,3 +137,17 @@ if __name__ == '__main__':
 
 #use ordered dict and docstrings to control the menu loop to run the methods that
 #we need to do
+
+# TODO
+# add looping abilities to the adding of a program so they can have 2 programs#
+#add a program, ask if theyre done adding a program#
+#if done, create the entry with name,program1 info, program2 info = to nothing#
+#if not done, ask for second program choice#
+# are you finished selecting a program loop#
+# print membership details with 1.name 2. programs 3. hotel memb numbers#
+#add the ability to create multiple passengers#
+#After we print membership details, loop back to main menu#
+#Purchase travel menu/options
+#select from the list of travelers
+#
+
